927aebc99b5c6135ddb7766bc0f1d204de6426d2
janet wambua <janet2wambua@gmail.com>
2025-10-07 11:01:25 +0300

    Fix FormikForm JSX issue and update imports

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,15 +1,16 @@
-import RegistrationForm from "./components/RegistrationForm";
+import FormikForm from "./components/formikForm.jsx";
 import "./styles/Form.css";
 
 function App() {
   return (
     <div className="app-container">
-      <h1>User Registration Form</h1>
-      <RegistrationForm />
+      <h1>User Registration Form (Formik)</h1>
+      <FormikForm />  {/* <- render the Formik form */}
     </div>
   );
 }
 
 export default App;
+
 
 
