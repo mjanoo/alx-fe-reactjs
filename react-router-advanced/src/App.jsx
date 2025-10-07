291b28fc48dd07cdf72fdefcddbce0c6dc9d7ef5
janet wambua <janet2wambua@gmail.com>
2025-10-07 13:06:08 +0300

    Moved Profile component to components folder for nested routes

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Profile from "./pages/Profile";
+import Profile from "./components/Profile";
 import BlogPost from "./pages/BlogPost";
 
 function App() {
